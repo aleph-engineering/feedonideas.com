d657ac2c6e511c5f267c9250f80399b493549233
reFucktor <hmvalcarcel@gmail.com>
2016-04-08 17:16:12 -0400

    Create user controller with function to check email in every profile.

---
+++ app/controllers/userController.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const userProfile = require('../models').UserProfile;
+
+const userController = {
+    /**
+     * Check if email exists in one of the current profiles(fb,ln,gh,g+)
+     * @param email
+     * @param callback
+     */
+    checkProfileExist: function(email, callback){
+        if (typeof callback === "function") {
+            userProfile.findOne({$or: [{'googleUser.email': email}, {'facebookUser.email': email}, {'linkedInUser.email': email}, {'gitHubUser.email': email}, {'localUser.email': email}]},
+                function (error, model) {
+                    callback(error,model);
+                });
+        }
+    }
+};
+module.exports = userController;

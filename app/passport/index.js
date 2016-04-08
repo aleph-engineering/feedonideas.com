0768d600742e333e2fac869c82769d375d28959d
reFucktor <hmvalcarcel@gmail.com>
2016-04-08 16:32:10 -0400

    Create main passport configuration.

---
+++ app/passport/index.js
@@ -0,0 +1,30 @@
+'use strict';
+const passport = require('passport'),
+    profile = require('../models').UserProfile;
+
+const passportConfig = function(app){
+
+    app.use(passport.initialize());
+    app.use(passport.session());
+
+    /*passport.serializeUser(localUser.serializeUser());
+     passport.deserializeUser(localUser.deserializeUser());
+     passport.deserializeUser(function(user, done){
+     done(null, user);
+     });*/
+    passport.serializeUser(function(user, done) {
+        done(null, user._id);
+    });
+    passport.deserializeUser(function(id, done) {
+        profile.findById(id, function(err, user) {
+            done(err, user);
+        });
+    });
+
+    require('./facebook')(app);
+    require('./google')(app);
+    require('./linkedin')(app);
+    require('./github')(app);
+};
+
+module.exports = passportConfig;

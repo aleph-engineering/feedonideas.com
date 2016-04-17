fda6350fb7c0cb6fe77d1aac82eb80c00bff0d91
reFucktor <hmvalcarcel@gmail.com>
2016-04-16 23:55:15 -0400

    New controller for topics.

---
+++ app/controllers/topicController.js
@@ -0,0 +1,48 @@
+'use strict';
+
+const models = require('../models'),
+    userProfile = models.UserProfile,
+    topic = models.Topic;
+
+var topicController = {
+    findTopicById(topicId, callback){
+        if (typeof callback === "function") {
+            topic.findById(topicId, function (error, model) {
+                callback(error, model);
+            })
+        }
+    },
+    getAllTopics(callback){
+        if (typeof callback === "function") {
+            topic.find(function(error, model){
+                callback(error, model);
+            })
+        }
+    },
+    getUserTopics(userId, callback){
+        if (typeof callback === "function") {
+            topic.find({authorId: userId}, function(error, model){
+                callback(error, model);
+            })
+        }
+    },
+    saveNewTopic(name, userId, pictureUrl, siteUrl, description, callback){
+        if (typeof callback === "function") {
+            var newTopic = new topic({
+                name: name,
+                roomId: Math.round((Math.random() * 1000000000)),
+                authorId: userId,
+                pictureUrl: pictureUrl,
+                siteUrl: siteUrl,
+                topicDesc: description
+            });
+            newTopic.save(function(error, model){
+                callback(error, model);
+            })
+        }
+    }
+
+
+};
+
+module.exports = topicController;

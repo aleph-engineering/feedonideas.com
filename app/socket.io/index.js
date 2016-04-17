4d57d672806884ec9a0136e2699f76e6cf1a6dcd
reFucktor <hmvalcarcel@gmail.com>
2016-04-16 23:51:12 -0400

    Configuriong socket.io module with function to get the rooms.

--- app/socket.io/index.js
+++ app/socket.io/index.js
@@ -1,8 +1,26 @@
 'use strict';
 const userProfile = require('../models').UserProfile;
 
-const socketConfig = function(socket){
-    socket.on('voteUp')
+const socketConfig = function(io, socket){
+    socket.emit('ready', {voteUp: 10});
 };
+function findClientsSocket(io,roomId, namespace) {
+    var res = [],
+        ns = io.of(namespace ||"/");    // the default namespace is "/"
 
+    if (ns) {
+        for (var id in ns.connected) {
+            if(roomId) {
+                var index = ns.connected[id].rooms.indexOf(roomId) ;
+                if(index !== -1) {
+                    res.push(ns.connected[id]);
+                }
+            }
+            else {
+                res.push(ns.connected[id]);
+            }
+        }
+    }
+    return res;
+}
 module.exports = socketConfig;

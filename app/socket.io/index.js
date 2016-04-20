d83c6f3c48b71c508a19240d954d3700de0f446a
reFucktor <hmvalcarcel@gmail.com>
2016-04-20 00:50:24 -0400

    New events for enter in feeds room.

--- app/socket.io/index.js
+++ app/socket.io/index.js
@@ -1,8 +1,39 @@
 'use strict';
-const userProfile = require('../models').UserProfile;
+const models = require('../models'),
+    controllers = require('../controllers'),
+    userProfile = models.UserProfile,
+    userController = controllers.userController,
+    topicController = controllers.topicController,
+    feedController = controllers.feedController;
 
-const socketConfig = function(io, socket){
-    socket.emit('ready', {voteUp: 10});
+const socketConfig = function(io, socket, userId){
+
+    socket.on('enterFeedsRoom', function(data){
+        var room = findClientsSocket(io,data.roomId);
+        if(room.length < 100){
+            socket.join(data.roomId);
+        }
+    });
+    socket.on('createFeed', function(data){
+        var room = findClientsSocket(io, data.roomId);
+        feedController.saveNewFeedWithRoomId(data.roomId, socket.userId, data.feedBody, function(error, model){
+            console.log("ERROR: " + error);
+            io.in(data.roomId).emit('feedCreated', {feed: model});
+        })
+    });
+    socket.on('enter-topic', function(data){
+        var room = findClientsSocket(io, data.id);
+        socket.userId = userController.getUserById(user)
+    });
+
+    socket.on('voteUp', function(data){
+        var room = findClientsSocket(io, data.id);
+        feedController.setUp(data.feed, userId, function(error, model){
+            if(!error){
+                io.in(data.id).emit('setVoteUp', {feedId: model._id});
+            }
+        });
+    });
 };
 function findClientsSocket(io,roomId, namespace) {
     var res = [],
@@ -11,8 +42,8 @@ function findClientsSocket(io,roomId, namespace) {
     if (ns) {
         for (var id in ns.connected) {
             if(roomId) {
-                var index = ns.connected[id].rooms.indexOf(roomId) ;
-                if(index !== -1) {
+                var index = ns.connected[id].rooms[roomId];
+                if(index) {
                     res.push(ns.connected[id]);
                 }
             }

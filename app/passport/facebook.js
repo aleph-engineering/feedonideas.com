34e67460be8c657a71371942872360324a4cfba6
reFucktor <hmvalcarcel@gmail.com>
2016-04-23 03:15:30 -0400

    Improving facebook authentication.

--- app/passport/facebook.js
+++ app/passport/facebook.js
@@ -11,7 +11,7 @@ const facebookConfig = function(app){
             clientID: (process.env.FACEBOOK_APP_ID || ""),
             clientSecret: (process.env.FACEBOOK_APP_SECRET || ""),
             callbackURL: "/auth/facebook/callback",
-            profileFields: ['id','displayName', 'picture', 'gender' ,'email'],
+            profileFields: ['id','displayName', 'picture.type(large)', 'gender' ,'email'],
             enableProof: true
         },
         function(accessToken, refreshToken, profile, done) {
@@ -25,13 +25,14 @@ const facebookConfig = function(app){
                             name : profile._json.name,
                             email: profile._json.email,
                             gender: profile._json.gender,
-                            avatar: profile._json.picture.data.url
+                            avatarUrl: profile._json.picture.data.url
                         });
 
                         userController.checkProfileExist(profile._json.email,function(error,model){
                             if(error) { return done(null, null) }
                             if(model){
                                 model.facebookUser = newFacebookUser;
+                                model.loginAvatarUrl= profile._json.picture.data.url;
                                 model.save(function(err){
                                     if(!err) return done(null, model);
                                     else{
@@ -40,7 +41,8 @@ const facebookConfig = function(app){
                             }
                             else{
                                 var newProfile = new userProfile({
-                                    name : profile._json.displayName
+                                    name : profile._json.name,
+                                    loginAvatarUrl: profile._json.picture.data.url
                                 });
                                 newProfile.facebookUser = newFacebookUser;
                                 newProfile.save(function(err){
@@ -59,6 +61,7 @@ const facebookConfig = function(app){
 
     app.get('/auth/facebook/callback', passport.authenticate('facebook', { failureRedirect: '/login' }),
         function(req, res) {
+            res.cookie('provider', 'facebook');
             res.redirect('/');
         });
 };

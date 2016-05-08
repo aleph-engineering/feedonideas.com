d687db2679ab10282c840e49faac2a42c7e272b8
reFucktor <hmvalcarcel@gmail.com>
2016-05-08 12:39:01 -0400

    socket.io: Remove unused method to find socket in the same room.

--- app/socket.io/index.js
+++ app/socket.io/index.js
@@ -9,23 +9,4 @@ const models = require('../models'),
 var socketConfig = function(io, socket, profile){
     require('./feedsSocket')(io, socket, profile);
 };
-function findClientsSocket(io,roomId, namespace) {
-    var res = [],
-        ns = io.of(namespace ||"/");    // the default namespace is "/"
-
-    if (ns) {
-        for (var id in ns.connected) {
-            if(roomId) {
-                var index = ns.connected[id].rooms[roomId];
-                if(index) {
-                    res.push(ns.connected[id]);
-                }
-            }
-            else {
-                res.push(ns.connected[id]);
-            }
-        }
-    }
-    return res;
-}
 module.exports = socketConfig;
